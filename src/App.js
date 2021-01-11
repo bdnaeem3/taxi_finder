1ece5f03998f46227ddfe2ff181023b725521885
Naeem Ahmed <naeem.ahmed@portonics.com>
2021-01-11 11:45:49 +0600

    Pythagorean theorem applyed for location

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Jumbotron, Container, Button, Form, FormGroup, Input, Table } from 'reactstrap';
+import _ from 'lodash';
 
 // CSS
 import './styles/App.scss';
 
 // Actions
 import { getCityListAction } from './redux/actions/mapActions';
-import { searchTaxiListAction } from './redux/actions/taxiActions';
+import { taxiListAction } from './redux/actions/taxiActions';
 
 
 const App = () => {
@@ -15,10 +16,15 @@ const App = () => {
   const dispatch = useDispatch();
   const cities = useSelector(state=>state.map.cities)
   const taxis = useSelector(state=>state.taxi.taxis)
+  const [ searchList, setSearchList ] = useState([])
   const [ formDatas, setFormDatas ] = useState({
     city: '',
     type: ''
   })
+
+  const locationCalc = location => {
+    return Math.sqrt(Math.pow(location.lat, 2) + Math.pow(location.lang, 2));
+  }
 
   const formChangeHandler = e => {
     setFormDatas({
@@ -29,8 +35,19 @@ const App = () => {
 
   const taxiSearchHandler = e => {
     e.preventDefault();
-    searchTaxiListAction(dispatch, formDatas)
+
+    setSearchList(taxis.filter(taxi=>{
+      return (
+        !taxi.booked && 
+        _.inRange(locationCalc(taxi.location), parseFloat(formDatas.city)-.1, parseFloat(formDatas.city)+.1) && 
+        taxi.type === formDatas.type
+      )
+    }))
   }
+
+  useEffect(()=>{
+    taxiListAction(dispatch)
+  }, [dispatch])
 
   useEffect(()=>{
     getCityListAction(dispatch)
@@ -47,7 +64,7 @@ const App = () => {
                 {
                   cities.length > 0
                     ? cities.map((city, i)=>{
-                      return <option key={i}>{city.name}</option>
+                      return <option value={locationCalc(city.location)} key={i}>{city.name}</option>
                     })
                     : 'No city found'
                 }
@@ -65,6 +82,42 @@ const App = () => {
         </Jumbotron>
 
         {
+          searchList.length > 0 &&
+          <>
+            <h1>Searched Taxis</h1>
+            <Table striped bordered>
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>Car Name</th>
+                  <th>Type</th>
+                  <th>Driver Name</th>
+                  <th>Number</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                {
+                  searchList.map((taxi, i)=> {
+                    return (
+                      <tr key={i}>
+                        <th scope="row">{i+1}</th>
+                        <td>{taxi.title}</td>
+                        <td>{taxi.type}</td>
+                        <td>{taxi.driver}</td>
+                        <td>{taxi.number}</td>
+                        <td><Button>Book Now</Button></td>
+                      </tr>
+                    )
+                  })
+                }
+              </tbody>
+            </Table>
+          </>
+        }
+
+        {/* <h1>Taxi List</h1>
+        {
           taxis.length > 0 &&
           <Table striped bordered>
             <thead>
@@ -74,6 +127,7 @@ const App = () => {
                 <th>Type</th>
                 <th>Driver Name</th>
                 <th>Number</th>
+                <th>Avaiable At</th>
                 <th>Action</th>
               </tr>
             </thead>
@@ -87,6 +141,7 @@ const App = () => {
                       <td>{taxi.type}</td>
                       <td>{taxi.driver}</td>
                       <td>{taxi.number}</td>
+                      <td>{locationCalc(taxi.location)}</td>
                       <td><Button>Book Now</Button></td>
                     </tr>
                   )
@@ -95,6 +150,40 @@ const App = () => {
             </tbody>
           </Table>
         }
+
+        
+        <h1>Map List</h1>
+        {
+          cities.length > 0 &&
+          <Table striped bordered>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Name</th>
+                <th>Lat</th>
+                <th>Lang</th>
+                <th>Area</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {
+                cities.map((city, i)=> {
+                  return (
+                    <tr key={i}>
+                      <th scope="row">{i+1}</th>
+                      <td>{city.name}</td>
+                      <td>{city.location.lat}</td>
+                      <td>{city.location.lang}</td>
+                      <td>{locationCalc(city.location)}</td>
+                      <td><Button>Book Now</Button></td>
+                    </tr>
+                  )
+                })
+              }
+            </tbody>
+          </Table>
+        } */}
       </Container>
     </main>
   );
--- src/redux/actions/taxiActions.js
+++ src/redux/actions/taxiActions.js
@@ -1,19 +1,13 @@
 import axios from 'axios';
 import { TAXI_SEARCH } from '../types/types';
 
-export const searchTaxiListAction = (dispatch, formDatas) => {
+
+export const taxiListAction = (dispatch) => {
   return axios.get('taxi.json')
     .then(response=>{
-
-      // Filering with the city and type inputted by the search form
-      // We could use the city and type as peremeter with API
-      const filteredTaxis = response.data.taxis.filter(taxi=>{
-        return !taxi.booked && taxi.available_at === formDatas.city && taxi.type === formDatas.type
-      })
-
       dispatch({
         type: TAXI_SEARCH,
-        data: filteredTaxis
+        data: response.data.taxis
       })
     })
     .catch(() => {
